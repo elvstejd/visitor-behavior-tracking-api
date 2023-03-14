03a0ac71fa0b0080c01c863e40eec951f8dab512
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-03-14 11:17:07 -0400

    Initial commit

---
+++ src/main/java/com/elvstejd/visitorbehaviortrackingapi/VisitorBehaviorTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.elvstejd.visitorbehaviortrackingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorBehaviorTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorBehaviorTrackingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elvstejd/visitorbehaviortrackingapi/VisitorBehaviorTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvstejd.visitorbehaviortrackingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorBehaviorTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
